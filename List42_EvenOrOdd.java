ad5ed398097e422c83810dba2876185f36bc99bf
Davi Macedo <134751279+DaviMacedo07@users.noreply.github.com>
2024-09-02 11:06:58 -0300

    --

--- List42_EvenOrOdd.java
+++ List42_EvenOrOdd.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
+import java.util.Random;
+
 public class List42_EvenOrOdd {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         int[] numbers = new int[20];
         int[] pares = new int[20];
@@ -9,8 +10,8 @@ public class List42_EvenOrOdd {
         int countPares = 0, countImpares = 0;
 
         for (int i = 0; i <= 19 ; i++) {
-            System.out.println("Digite um numero: ");
-            numbers[i] = scanner.nextInt();
+
+            numbers[i] = random.nextInt(100);
 
             if (numbers[i] % 2 == 0) {
                 pares[countPares] = numbers[i];
@@ -19,7 +20,19 @@ public class List42_EvenOrOdd {
                 impares[countImpares] = numbers[i];
                 countImpares++;
             }
+        }
+        System.out.println("Todos os números: ");
+        for (int i = 0; i < 19; i++) {
+            System.out.print(numbers[i] + " ,");
+        }
+        System.out.println("Pares: \n");
+        for (int i = 0; i <= countPares ;i++) {
+            System.out.print(pares[i] + " ,");
+        }
 
+        System.out.println("Impares: \n");
+        for (int i = 0; i <= countImpares; i++) {
+            System.out.print(impares[i] + " ,");
         }
     }
 }
