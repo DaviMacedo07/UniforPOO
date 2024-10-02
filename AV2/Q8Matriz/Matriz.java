62b8a7fbfb5dbf6b96334bd8f54cfcd565d766ee
Davi Macedo <134751279+DaviMacedo07@users.noreply.github.com>
2024-10-02 15:02:42 -0300

    ---

--- AV2/Q8Matriz/Matriz.java
+++ AV2/Q8Matriz/Matriz.java
@@ -7,7 +7,7 @@ public class Matriz {
     private int colunas;
     private int [][]matriz;
 
-    public Matriz(int linhas, int colunas, int[][] matriz) {
+    public Matriz(int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
         this.matriz = new int [linhas][colunas];
@@ -20,11 +20,55 @@ public class Matriz {
         System.out.println("Quantas colunas serão: ");
         this.colunas = scanner.nextInt();
 
+        matriz = new int [linhas][colunas];
+
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
                 System.out.println("Digite o indice da matriz na posição: [ " + i + " ][ " + j + " ]");
                 matriz[i][j] = scanner.nextInt();
             }
         }
+        System.out.println("Matriz alimentada! ");
     }
-}
+
+    public void mostrarMatriz() {
+                System.out.println("Matriz:");
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    System.out.print(matriz[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public Matriz somar (Matriz outra) {
+            Matriz resultado = new Matriz(this.linhas, this.colunas);
+            for (int i = 0; i < this.linhas; i++) {
+                for (int j = 0; j < this.colunas; j++) {
+                    resultado.matriz[i][j] = this.matriz[i][j] + outra.matriz[i][j];
+                }
+            }
+            return resultado;
+        }
+
+        public boolean equalsMatrizes(Matriz outra) {
+            for (int i = 0; i < this.linhas; i++) {
+                for (int j = 0; j < this.colunas; j++) {
+                    if (this.matriz[i][j] != outra.matriz[i][j]) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    public Matriz transpor() {
+        Matriz transposta = new Matriz(this.colunas, this.linhas);
+        for (int i = 0; i < this.linhas; i++) {
+            for (int j = 0; j < this.colunas; j++) {
+                transposta.matriz[j][i] = this.matriz[i][j];
+            }
+        }
+        return transposta;
+    }
+    }
--- AV2/Q8Matriz/TesteMatriz.java
+++ AV2/Q8Matriz/TesteMatriz.java
@@ -1,4 +1,26 @@
 package AV2.Q8Matriz;
 
 public class TesteMatriz {
+    public static void main(String[] args) {
+        Matriz matriz = new Matriz(0, 0);
+       // Matriz segundaMatriz = new Matriz(0, 0);
+        matriz.alimentarMatriz();
+     //   segundaMatriz.alimentarMatriz();
+        matriz.mostrarMatriz();
+     //   segundaMatriz.mostrarMatriz();
+     //   Matriz resultadoSoma = matriz.somar(segundaMatriz);
+        Matriz transposta = matriz.transpor();
+
+
+     //   System.out.println("Resultado da soma das duas matrizes");
+      //  resultadoSoma.mostrarMatriz();
+     //   if (matriz.equalsMatrizes(segundaMatriz)) {
+      //      System.out.println("As matrizes são idênticas.");
+     //   } else {
+    //        System.out.println("As matrizes são diferentes.");
+    //    }
+
+        System.out.println("Matriz transposta: ");
+        transposta.mostrarMatriz();
+    }
 }
