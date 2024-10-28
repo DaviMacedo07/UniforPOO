50e9e09acc44129440c1cf13dd2fa216ec4a9f5a
Davi Macedo <134751279+DaviMacedo07@users.noreply.github.com>
2024-10-28 07:49:11 -0300

    ---

--- AV2/Q8Matriz/Matriz.java
+++ AV2/Q8Matriz/Matriz.java
@@ -1,76 +1,148 @@
 package AV2.Q8Matriz;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Matriz {
+    private int[][] elementos;
     private int linhas;
     private int colunas;
-    private int [][]matriz;
 
     public Matriz(int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
-        this.matriz = new int [linhas][colunas];
+        this.elementos = new int[linhas][colunas];
     }
 
-    public void alimentarMatriz() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite quantas linhas serão: ");
-        this.linhas = scanner.nextInt();
-        System.out.println("Quantas colunas serão: ");
-        this.colunas = scanner.nextInt();
+    public int getElemento(int linha, int coluna) {
+        return elementos[linha][coluna];
+    }
 
-        matriz = new int [linhas][colunas];
+    public void setElemento(int linha, int coluna, int valor) {
+        elementos[linha][coluna] = valor;
+    }
 
+    public Matriz adicionar(Matriz outra) {
+        if (this.linhas != outra.linhas || this.colunas != outra.colunas) {
+            throw new IllegalArgumentException("Matrizes devem ter as mesmas dimensões para adição.");
+        }
+        Matriz resultado = new Matriz(linhas, colunas);
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
-                System.out.println("Digite o indice da matriz na posição: [ " + i + " ][ " + j + " ]");
-                matriz[i][j] = scanner.nextInt();
+                resultado.setElemento(i, j, this.getElemento(i, j) + outra.getElemento(i, j));
             }
         }
-        System.out.println("Matriz alimentada! ");
+        return resultado;
     }
 
-    public void mostrarMatriz() {
-                System.out.println("Matriz:");
-            for (int i = 0; i < linhas; i++) {
-                for (int j = 0; j < colunas; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.println();
+    public boolean equals(Matriz outra) {
+        if (this.linhas != outra.linhas || this.colunas != outra.colunas) {
+            return false;
+        }
+        for (int i = 0; i < linhas; i++) {
+            if (!Arrays.equals(this.elementos[i], outra.elementos[i])) {
+                return false;
             }
         }
+        return true;
+    }
 
-        public Matriz somar (Matriz outra) {
-            Matriz resultado = new Matriz(this.linhas, this.colunas);
-            for (int i = 0; i < this.linhas; i++) {
-                for (int j = 0; j < this.colunas; j++) {
-                    resultado.matriz[i][j] = this.matriz[i][j] + outra.matriz[i][j];
-                }
-            }
-            return resultado;
-        }
-
-        public boolean equalsMatrizes(Matriz outra) {
-            for (int i = 0; i < this.linhas; i++) {
-                for (int j = 0; j < this.colunas; j++) {
-                    if (this.matriz[i][j] != outra.matriz[i][j]) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
-    public Matriz transpor() {
-        Matriz transposta = new Matriz(this.colunas, this.linhas);
-        for (int i = 0; i < this.linhas; i++) {
-            for (int j = 0; j < this.colunas; j++) {
-                transposta.matriz[j][i] = this.matriz[i][j];
+    public Matriz transposta() {
+        Matriz transposta = new Matriz(colunas, linhas);
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                transposta.setElemento(j, i, this.getElemento(i, j));
             }
         }
         return transposta;
     }
 
-
+    public Matriz oposta() {
+        Matriz oposta = new Matriz(linhas, colunas);
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                oposta.setElemento(i, j, -this.getElemento(i, j));
+            }
+        }
+        return oposta;
     }
+
+    public static Matriz gerarNula(int linhas, int colunas) {
+        return new Matriz(linhas, colunas);  // Por padrão, todos os elementos são 0
+    }
+
+    public boolean isIdentidade() {
+        if (linhas != colunas) return false;
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if ((i == j && elementos[i][j] != 1) || (i != j && elementos[i][j] != 0)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isDiagonal() {
+        if (linhas != colunas) return false;
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (i != j && elementos[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isSimetrica() {
+        return this.equals(this.transposta());
+    }
+
+    public boolean isAntiSimetrica() {
+        return this.equals(this.oposta().transposta());
+    }
+
+    public Matriz subtrair(Matriz outra) {
+        if (this.linhas != outra.linhas || this.colunas != outra.colunas) {
+            throw new IllegalArgumentException("Matrizes devem ter as mesmas dimensões para subtração.");
+        }
+        Matriz resultado = new Matriz(linhas, colunas);
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                resultado.setElemento(i, j, this.getElemento(i, j) - outra.getElemento(i, j));
+            }
+        }
+        return resultado;
+    }
+
+    public Matriz multiplicar(Matriz outra) {
+        if (this.colunas != outra.linhas) {
+            throw new IllegalArgumentException("Número de colunas da primeira matriz deve ser igual ao número de linhas da segunda.");
+        }
+        Matriz resultado = new Matriz(linhas, outra.colunas);
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < outra.colunas; j++) {
+                int soma = 0;
+                for (int k = 0; k < colunas; k++) {
+                    soma += this.getElemento(i, k) * outra.getElemento(k, j);
+                }
+                resultado.setElemento(i, j, soma);
+            }
+        }
+        return resultado;
+    }
+
+    public Matriz copiar() {
+        Matriz copia = new Matriz(linhas, colunas);
+        for (int i = 0; i < linhas; i++) {
+            System.arraycopy(this.elementos[i], 0, copia.elementos[i], 0, colunas);
+        }
+        return copia;
+    }
+
+    public void exibir() {
+        for (int[] linha : elementos) {
+            System.out.println(Arrays.toString(linha));
+        }
+    }
+}
--- AV2/Q8Matriz/TesteMatriz.java
+++ AV2/Q8Matriz/TesteMatriz.java
@@ -2,25 +2,65 @@ package AV2.Q8Matriz;
 
 public class TesteMatriz {
     public static void main(String[] args) {
-        Matriz matriz = new Matriz(0, 0);
-       // Matriz segundaMatriz = new Matriz(0, 0);
-        matriz.alimentarMatriz();
-     //   segundaMatriz.alimentarMatriz();
-        matriz.mostrarMatriz();
-     //   segundaMatriz.mostrarMatriz();
-     //   Matriz resultadoSoma = matriz.somar(segundaMatriz);
-        Matriz transposta = matriz.transpor();
 
+        Matriz matrizA = new Matriz(3, 3);
+        matrizA.setElemento(0, 0, 1);
+        matrizA.setElemento(0, 1, 2);
+        matrizA.setElemento(0, 2, 3);
+        matrizA.setElemento(1, 0, 4);
+        matrizA.setElemento(1, 1, 5);
+        matrizA.setElemento(1, 2, 6);
+        matrizA.setElemento(2, 0, 7);
+        matrizA.setElemento(2, 1, 8);
+        matrizA.setElemento(2, 2, 9);
 
-     //   System.out.println("Resultado da soma das duas matrizes");
-      //  resultadoSoma.mostrarMatriz();
-     //   if (matriz.equalsMatrizes(segundaMatriz)) {
-      //      System.out.println("As matrizes são idênticas.");
-     //   } else {
-    //        System.out.println("As matrizes são diferentes.");
-    //    }
+        System.out.println("Matriz A:");
+        matrizA.exibir();
 
-        System.out.println("Matriz transposta: ");
-        transposta.mostrarMatriz();
+        Matriz matrizB = new Matriz(3, 3);
+        matrizB.setElemento(0, 0, 9);
+        matrizB.setElemento(0, 1, 8);
+        matrizB.setElemento(0, 2, 7);
+        matrizB.setElemento(1, 0, 6);
+        matrizB.setElemento(1, 1, 5);
+        matrizB.setElemento(1, 2, 4);
+        matrizB.setElemento(2, 0, 3);
+        matrizB.setElemento(2, 1, 2);
+        matrizB.setElemento(2, 2, 1);
+
+        System.out.println("\nMatriz B:");
+        matrizB.exibir();
+
+        Matriz soma = matrizA.adicionar(matrizB);
+        System.out.println("\nA + B:");
+        soma.exibir();
+
+        Matriz subtracao = matrizA.subtrair(matrizB);
+        System.out.println("\nA - B:");
+        subtracao.exibir();
+
+        Matriz multiplicacao = matrizA.multiplicar(matrizB);
+        System.out.println("\nA * B:");
+        multiplicacao.exibir();
+
+        System.out.println("\nA é simétrica? " + matrizA.isSimetrica());
+
+        System.out.println("A é identidade? " + matrizA.isIdentidade());
+
+        Matriz transpostaA = matrizA.transposta();
+        System.out.println("\nTransposta de A:");
+        transpostaA.exibir();
+
+        Matriz opostaA = matrizA.oposta();
+        System.out.println("\nOposta de A:");
+        opostaA.exibir();
+
+        Matriz nula = Matriz.gerarNula(2, 2);
+        System.out.println("\nMatriz Nula 2x2:");
+        nula.exibir();
+
+        Matriz copiaA = matrizA.copiar();
+        System.out.println("\nCópia de A:");
+        copiaA.exibir();
     }
 }
